05a9230b8650f74a7e933c10890fc082376998ea
Ms-Shoshany <channashosh@gmail.com>
2025-05-26 15:08:34 +0300

    Add file via API

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/hello", methods=["GET"])
+def hello():
+	data = request.json
+	return jsonify(f"reply from endpoint hello, data = {data}")
+
+@app.route("/hello", methods=["GET"])
+def hello():
+	return jsonify(f"reply from endpoint hello")
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", debug=True)
